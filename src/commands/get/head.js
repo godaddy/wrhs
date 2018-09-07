7c6c457c0b115f9246c7ff6da08698b90d254779
Geoff Wall <gjwall@asu.edu>
2018-09-06 17:16:00 -0700

    [tiny] Added fallback width if terminal is super tiny

--- src/commands/get/head.js
+++ src/commands/get/head.js
@@ -32,7 +32,7 @@ class HeadCommand extends Command {
 
         response.forEach(build => {
           const titleText = ` ${chalk.green.bold(build.name)} | ${chalk.green(build.env)} | ${build.version} | ${build.locale} `;
-          const width = (process.stdout.columns || 150) - titleText.length;
+          const width = Math.max((process.stdout.columns || 150) - titleText.length, 10);
           const titleBar = chalk.bgWhite(new Array(Math.floor(width / 2)).fill(' ').join(''));
 
           console.log(titleBar + titleText + titleBar);
