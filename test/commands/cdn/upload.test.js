946017c1d9ca8cfef3a764b0a22a0592a3c1d084
Jacopo Daeli <jacopo.daeli@gmail.com>
2020-12-16 13:50:53 -0500

    lint

--- test/commands/cdn/upload.test.js
+++ test/commands/cdn/upload.test.js
@@ -3,12 +3,7 @@ const { expect, test } = require('@oclif/test');
 describe('upload', () => {
   test
     .stdout()
-    .command([
-      'upload',
-      '/dev/myFilesFolder',
-      '--expiration',
-      '365d'
-    ])
+    .command(['upload', '/dev/myFilesFolder', '--expiration', '365d'])
     .it('runs cdn:upload /dev/myFilesFolder', (ctx) => {
       expect(ctx.stdout).to.contain('This is not implemented yet');
     });
